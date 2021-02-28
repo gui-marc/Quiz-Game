81f357bcdc5b4bb2f7325330ca14e948476faeb4
Guilherme Marcondes <guisupre@gmail.com>
2021-02-28 19:42:23 -0300

    Implemented Delete (CRUD)

--- backend/src/main/java/com/guilherme/backend/controller/QuestionController.java
+++ backend/src/main/java/com/guilherme/backend/controller/QuestionController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,6 @@ public class QuestionController {
     private QuestionRepository questionRepository;
 
     @GetMapping
-    @ResponseStatus(HttpStatus.FOUND)
     public List<Question> getQuestions() {
         return questionRepository.findAll();
     }
@@ -31,6 +32,11 @@ public class QuestionController {
     @ResponseStatus(HttpStatus.CREATED)
     public Question addQuestion(@RequestBody Question question) {
         return questionRepository.save(question);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteQuestion(@PathVariable Long id) {
+        questionRepository.deleteById(id);
     }
 
 }
