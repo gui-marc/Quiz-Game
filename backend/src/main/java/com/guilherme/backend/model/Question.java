92b7c97dc2c593d2458e897b68b3590d275d0245
Guilherme Marcondes <guisupre@gmail.com>
2021-02-28 18:56:54 -0300

    Fixed: Question Model in Wrong Folder

---
+++ backend/src/main/java/com/guilherme/backend/model/Question.java
@@ -0,0 +1,25 @@
+package com.guilherme.backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // No real use in API
+
+    @Column(nullable = false)
+    private String heading;
+    @Column(nullable = false)
+    private String[] answers;
+    @Column(nullable = false)
+    private Integer answerIndex;
+}
