87b9ae6d6e421514c15dcd696fcd4ad69367b00b
Guilherme Marcondes <guisupre@gmail.com>
2021-03-07 00:28:40 -0300

    Populated Test Tables

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO QUESTION (heading, answer_index) VALUES ('Em javascript, qual a diferença entre Set e Map?', 2);
+INSERT INTO ALTERNATIVE (alternative, question_id) VALUES ('Alternativa 1', 1);
